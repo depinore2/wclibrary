cd0151009d96fee2d8fb0aafdbcd2158eb318655
Edgar Martinez <leetgardo@gmail.com>
2021-12-10 01:07:14 -0800

    Added a safer way to generate sanitized HTML.

--- src/BaseWebComponent.ts
+++ src/BaseWebComponent.ts
@@ -21,6 +21,8 @@ export abstract class BaseWebComponent extends HTMLElement
         else
             throw new Error('ShadowRoot did not initialize properly.');
     }
+
+    // prefer to use template() instead, as it's safer and simpler to use.
     protected render(content: string, selector: string = ''): void 
     {
         if(this.shadowRoot) {
@@ -41,14 +43,27 @@ export abstract class BaseWebComponent extends HTMLElement
             throw new Error('Shadow root unexectedly missing from element.');
     }
 
-    // use this to easily sanitize an entire html template string.
-    protected html(strings:TemplateStringsArray, ...expressions: string[]) {
-        let output = '';
-        for(let i = 0; i < strings.length; i++) {
-            output += strings[i] + this.sanitize(expressions[i]);
+    /* 
+        use this to easily sanitize an entire html template string.
+        example usage:
+            this.template('#myContainerDiv')`
+                <div>potentially unsafe value: ${someData.someValue}
+            `;
+
+            ^^^ notice that this.sanitize() didn't have to be called here.  It will "magically" sanitize any string interpolation.
+            More info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
+    */
+    protected template(containerElement = '') {
+        const self = this;
+        return function (strings: TemplateStringsArray, ...expressions: string[]) {
+            let output = '';
+            for (let i = 0; i < strings.length; i++) {
+                output += strings[i] + self.sanitize(expressions[i]);
+            }
+            self.render(output, containerElement);
         }
-        return output;
     }
+
     
     protected generateSanitizer(): (content: string) => string {
         return content => purify.sanitize(content, { });
